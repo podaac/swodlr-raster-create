39604043744273b01084f064ff56ef6a069719f3
Joshua Garde <jgarde@jpl.nasa.gov>
2023-04-13 13:39:48 -0700

    Improve SDS error handling and ES lookups

--- podaac/swodlr_raster_create/submit_raster.py
+++ podaac/swodlr_raster_create/submit_raster.py
@@ -1,15 +1,18 @@
 import logging
+from time import sleep
 from . import sds_statuses
 from .utils import (
-  get_param, mozart_client, load_json_schema, search_datasets
+    get_param, mozart_client, load_json_schema, search_datasets
 )
 
-STAGE           = __name__.rsplit('.', 1)[1]
+STAGE = __name__.rsplit('.', 1)[1]
 PCM_RELEASE_TAG = get_param('sds_pcm_release_tag')
+MAX_ATTEMPTS = int(get_param('sds_submit_max_attempts'))
+TIMEOUT = int(get_param('sds_submit_timeout'))
 
 validate_jobset = load_json_schema('jobset')
 raster_job_type = mozart_client.get_job_type(
-  f'job-SCIFLO_L2_HR_Raster:${PCM_RELEASE_TAG}'
+    f'job-SCIFLO_L2_HR_Raster:${PCM_RELEASE_TAG}'
 )
 raster_job_type.initialize()
 
@@ -21,6 +24,7 @@ def lambda_handler(event, _context):
 
     output = validate_jobset({'jobs': raster_jobs})
     return output
+
 
 def process_job(eval_job):
     if eval_job['job_status'] not in sds_statuses.SUCCESS:
@@ -38,31 +42,52 @@ def process_job(eval_job):
 
     state_config = search_datasets(state_config_id, False)
     if state_config is None:
-      raster_job.update(
-        job_status='job-failed',
-        errors=['Unable to find state config from submit_evaluate stage']
-      )
-      return raster_job
+        raster_job.update(
+            job_status='job-failed',
+            errors=['Unable to find state config from submit_evaluate stage']
+        )
+        return raster_job
 
-    params = [
-      'raster_resolution', 'output_sampling_grid_type',
-      'output_granule_extent_flag', 'utm_zone_adjust', 'mgrs_band_adjust'
+    passthru_params = [
+        'raster_resolution', 'output_sampling_grid_type',
+        'output_granule_extent_flag', 'utm_zone_adjust', 'mgrs_band_adjust'
     ]
-    input_params = {param: eval_job['metadata'][param] for param in params}
+    input_params = {
+        param: eval_job['metadata'][param]
+        for param in passthru_params
+    }
 
     # Input param conversions
     input_params['output_sampling_grid_type'] = \
-      input_params['output_sampling_grid_type'].lower()
+        input_params['output_sampling_grid_type'].lower()
     input_params['output_granule_extent_flag'] = \
-      1 if input_params['output_granule_extent_flag'] else 0
+        1 if input_params['output_granule_extent_flag'] else 0
 
     raster_job_type.set_input_dataset(state_config)
     raster_job_type.set_input_params(input_params)
-    sds_job = raster_job_type.submit_job(tag='sciflo_raster_otello_submit')
+
+    for i in range(1, MAX_ATTEMPTS + 1):
+        try:
+            sds_job = raster_job_type.submit_job(
+                tag='sciflo_raster_otello_submit'
+            )
+
+            raster_job.update(
+                job_id=sds_job.job_id,
+                job_status='job-queued',
+                metadata=eval_job['metadata']
+            )
+            return raster_job
+        except:
+            logging.exception(
+                'Job submission failed; attempt %d/%d',
+                i, MAX_ATTEMPTS
+            )
+
+        sleep(TIMEOUT)
 
     raster_job.update(
-        job_id = sds_job.job_id,
-        job_status = 'job-queued',
-        metadata = eval_job['metadata']
+        job_status='job-failed',
+        errors=['SDS failed to accept job']
     )
     return raster_job
