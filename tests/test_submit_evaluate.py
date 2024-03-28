53db21c0972ec9978c643ea2a66b3d38e2e4eaa4
Joshua Garde <jgarde@jpl.nasa.gov>
2024-03-28 00:35:27 -0700

    submit_evaluate tests

--- tests/test_submit_evaluate.py
+++ tests/test_submit_evaluate.py
@@ -1,8 +1,9 @@
+'''Tests the submit_evaluate module'''
 import json
 import os
 from pathlib import Path
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 with (
     patch('boto3.client'),
@@ -22,14 +23,11 @@ with (
 class TestSubmitEvaluate(TestCase):
     '''Tests for the submit_evaluate module'''
     data_path = Path(__file__).parent.joinpath('data')
-    valid_sqs_path = data_path.joinpath('valid_sqs.json')
-    invalid_sqs_path = data_path.joinpath('invalid_sqs.json')
-    with valid_sqs_path.open('r', encoding='utf-8') as f:
-        valid_sqs = json.load(f)
-    with invalid_sqs_path.open('r', encoding='utf-8') as f:
-        invalid_sqs = json.load(f)
-        
-    def test_valid_submit(self):
+    success_jobset_path = data_path.joinpath('success_jobset.json')
+    with success_jobset_path.open('r', encoding='utf-8') as f:
+        success_jobset = json.load(f)
+
+    def test_successful_submit(self):
         with (
             patch.dict(os.environ, {
                 'SWODLR_sds_host': 'http://sds-host.test/',
@@ -40,4 +38,83 @@ class TestSubmitEvaluate(TestCase):
                 'podaac.swodlr_raster_create.utilities.utils.search_datasets'
             ) as search_ds_mock
         ):
-            results = submit_evaluate.lambda_handler(self.valid_sqs, None)
+            # Setup mocks
+            submit_evaluate.raster_eval_job_type.submit_job.return_value = \
+                MagicMock(job_id='72c4b5a0-f772-4311-b78d-d0d947b5db11')
+            
+            # Lambda call
+            results = submit_evaluate.lambda_handler(self.success_jobset, None)
+
+            # Assertion checks
+            search_ds_mock.assert_called_once_with('SWOT_L2_HR_PIXCVec_001_002_006L_*')
+            submit_evaluate.raster_eval_job_type.submit_job.assert_called_once()
+
+            # Results check
+            self.assertDictEqual(results, {
+                'jobs': [{
+                    'stage': 'submit_evaluate',
+                    'product_id': '24168643-1002-45f5-a059-0b5266bc28f3',
+                    'job_id': '72c4b5a0-f772-4311-b78d-d0d947b5db11',
+                    'job_status': 'job-queued'
+                }],
+                'inputs': {
+                    '24168643-1002-45f5-a059-0b5266bc28f3': {
+                        'product_id': '24168643-1002-45f5-a059-0b5266bc28f3',
+                        'cycle': 1,
+                        'pass': 2,
+                        'scene': 3,
+                        'raster_resolution': 100,
+                        'output_sampling_grid_type': 'UTM', 
+                        'output_granule_extent_flag': True,
+                        'utm_zone_adjust': 0,
+                        'mgrs_band_adjust': 0
+                    }
+                }
+            })
+
+    def test_not_found_submit(self):
+        with (
+            patch.dict(os.environ, {
+                'SWODLR_sds_host': 'http://sds-host.test/',
+                'SWODLR_sds_grq_es_path': '/grq_es',
+                'SWODLR_sds_grq_es_index': 'grq'
+            }),
+            patch(
+                'podaac.swodlr_raster_create.utilities.utils.search_datasets'
+            ) as search_ds_mock
+        ):
+            # Setup mocks
+            search_ds_mock.return_value = None
+            
+            # Lambda call
+            results = submit_evaluate.lambda_handler(self.success_jobset, None)
+
+            # Assertion checks
+            search_ds_mock.assert_called_once_with('SWOT_L2_HR_PIXCVec_001_002_006L_*')
+            submit_evaluate.raster_eval_job_type.submit_job.assert_not_called()
+
+            # Results check
+            self.assertDictEqual(results, {
+                'jobs': [{
+                    'stage': 'submit_evaluate',
+                    'product_id': '24168643-1002-45f5-a059-0b5266bc28f3',
+                    'job_status': 'job-failed',
+                    'errors': ['Scene does not exist']
+                }],
+                'inputs': {
+                    '24168643-1002-45f5-a059-0b5266bc28f3': {
+                        'product_id': '24168643-1002-45f5-a059-0b5266bc28f3',
+                        'cycle': 1,
+                        'pass': 2,
+                        'scene': 3,
+                        'raster_resolution': 100,
+                        'output_sampling_grid_type': 'UTM', 
+                        'output_granule_extent_flag': True,
+                        'utm_zone_adjust': 0,
+                        'mgrs_band_adjust': 0
+                    }
+                }
+            })
+            
+    def tearDown(self):
+        submit_evaluate.raster_eval_job_type.reset_mock()
